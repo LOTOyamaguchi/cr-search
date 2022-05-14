0e18dc12c6fe2d2e20ab44431aae8efed2e813b8
LOTOyamaguchi <13836268+LOTOyamaguchi@users.noreply.github.com>
2022-05-14 11:39:21 +0900

    テンプレートから改造する方針に変更。とりあえず仮実装

---
+++ src/__mocks__/decks.js
@@ -0,0 +1,36 @@
+import { v4 as uuid } from 'uuid';
+
+export const decks = [
+  {
+    id: uuid(),
+    cards: [
+      '/static/images/avatars/avatar_3.png',
+      '/static/images/avatars/avatar_3.png',
+      '/static/images/avatars/avatar_3.png',
+      '/static/images/avatars/avatar_3.png',
+      '/static/images/avatars/avatar_3.png',
+      '/static/images/avatars/avatar_3.png',
+      '/static/images/avatars/avatar_3.png',
+      '/static/images/avatars/avatar_3.png'
+    ],
+    name: '枯渇',
+    pattern: 'アウトロープリンス型',
+    averageElixir: '3.4'
+  },
+  {
+    id: uuid(),
+    cards: [
+      '/static/images/avatars/avatar_4.png',
+      '/static/images/avatars/avatar_4.png',
+      '/static/images/avatars/avatar_4.png',
+      '/static/images/avatars/avatar_4.png',
+      '/static/images/avatars/avatar_4.png',
+      '/static/images/avatars/avatar_4.png',
+      '/static/images/avatars/avatar_4.png',
+      '/static/images/avatars/avatar_4.png'
+    ],
+    name: '枯渇',
+    pattern: 'アウトロープリンス型',
+    averageElixir: '3.4'
+  }
+];
